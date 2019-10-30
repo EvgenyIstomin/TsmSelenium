6d4d3285423b71065594dda0f7aa2472406032a4
Evgeny Istomin <istomin.ev@gmail.com>
2019-10-30 14:30:03 -0500

    Created conftest file with @py.test.fixture

--- fixture/application.py
+++ fixture/application.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.support.ui import Select
 from fixture.session import SessionHelper
 
 
